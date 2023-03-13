50a4a2e9b37740c91be762d2d89867a37f4a18aa
LimGayoung <ygy920@gmail.com>
2023-03-13 15:22:05 +0900

    [Add]#1 - create spring project

---
+++ src/test/java/org/im/interviewmate/InterviewmateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.im.interviewmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
