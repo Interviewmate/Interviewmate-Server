34820b2203685078ae5ca91b35bcbc0c02d0973b
parkrootseok <parkrootseok@gmail.com>
2023-03-22 16:01:55 +0900

    [Add] #5 - CD 구현을 위한 파일 추가

---
+++ scripts/stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_ROOT="/home/ubuntu/app"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+CURRENT_PID=$(pgrep -f ".jar")
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]
+then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
