b908aa27f08ea6f8644801267a302d487299a56e
parkrootseok <parkrootseok@gmail.com>
2023-03-23 19:05:35 +0900

    [Add] #3 - 회원 가입 API Swagger 반영

---
+++ src/main/java/org/interviewmate/domain/user/api/UserApiController.java
@@ -0,0 +1,25 @@
+package org.interviewmate.domain.user.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.interviewmate.domain.user.dto.PostUserReq;
+import org.interviewmate.domain.user.dto.PostUserRes;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "유저 관련 API")
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserApiController {
+
+    @Operation(summary = "회원 가입 API", description = "필요한 정보를 받아 회원 가입 진행")
+    @PostMapping("/sign-up")
+    public PostUserRes signUp(@RequestBody PostUserReq postUserReq) {
+        return null;
+    }
+
+}
