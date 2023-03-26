feb0b1010b0269b2d2cf60f3240cbeaf90570a76
parkrootseok <parkrootseok@gmail.com>
2023-03-27 02:06:57 +0900

    [Add] #14 - toEntity 메소드

--- src/main/java/org/interviewmate/domain/user/model/dto/request/PostUserReq.java
+++ src/main/java/org/interviewmate/domain/user/model/dto/request/PostUserReq.java
@@ -2,10 +2,14 @@ package org.interviewmate.domain.user.model.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.interviewmate.domain.user.model.User;
 
 @Schema(name = "회원 가입 Request", description = "회원 가입에 필요한 유저에 대한 정보")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostUserReq {
 
     @Schema(description = "이메일", example = "moa.moa.interview@gmail.com")
@@ -19,7 +23,16 @@ public class PostUserReq {
     @Schema(description = "직무", example = "서버 개발자")
     private String job;
 
-    @Schema(description = "키워드", example = "{Spring, JPA, Java}")
+    @Schema(description = "키워드", example = "[\"Spring\", \"JPA\", \"Java\"]")
     private List<String> keyword;
+
+    public static User toEntity(PostUserReq postUserReq) {
+        return User.builder()
+                .email(postUserReq.getEmail())
+                .password(postUserReq.getPassword())
+                .nickName(postUserReq.getNickName())
+                .job(postUserReq.getJob())
+                .build();
+    }
 
 }
