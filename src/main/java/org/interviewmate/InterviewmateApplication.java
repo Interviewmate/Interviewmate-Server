65f360d4dd16828f6351f2e7386d901c483605e7
LimGayoung <ygy920@gmail.com>
2023-03-27 20:48:02 +0900

    [Feat]#20 - Base Entity 생성
    생성일, 수정일

--- src/main/java/org/interviewmate/InterviewmateApplication.java
+++ src/main/java/org/interviewmate/InterviewmateApplication.java
@@ -2,6 +2,7 @@ package org.interviewmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(
 		exclude = {
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
 		}
 )
+@EnableJpaAuditing
 public class InterviewmateApplication {
 
 	public static void main(String[] args) {
