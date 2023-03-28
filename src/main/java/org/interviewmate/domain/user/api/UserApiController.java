42cffabd0ccee50064956c55d90956621019d32e
parkrootseok <parkrootseok@gmail.com>
2023-03-28 09:07:16 +0900

    [Refactor] #14 - 공통 ResponseDto 적용

--- src/main/java/org/interviewmate/domain/user/api/UserApiController.java
+++ src/main/java/org/interviewmate/domain/user/api/UserApiController.java
@@ -1,11 +1,16 @@
 package org.interviewmate.domain.user.api;
 
+import static org.interviewmate.global.util.response.ResponseCode.*;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.interviewmate.domain.user.model.dto.request.PostUserReqDto;
 import org.interviewmate.domain.user.model.dto.response.PostUserResDto;
 import org.interviewmate.domain.user.service.UserService;
+import org.interviewmate.global.util.response.ResponseCode;
+import org.interviewmate.global.util.response.ResponseUtil;
+import org.interviewmate.global.util.response.dto.ResponseDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserApiController {
 
+    private final ResponseUtil responseUtil;
     private final UserService userService;
 
     @Operation(summary = "회원 가입 API", description = "필요한 정보를 받아 회원 가입 진행")
     @PostMapping("/sign-up")
-    // todo: valid 처리 및 공통 Response 객체 적용
-    public PostUserResDto signUp(@RequestBody PostUserReqDto postUserReqDto) {
-        return userService.createUser(postUserReqDto);
+    // todo: valid 처리
+    public ResponseDto<PostUserResDto> signUp(@RequestBody PostUserReqDto postUserReqDto) {
+        return responseUtil.SUCCESS(SUCCESS ,userService.createUser(postUserReqDto));
     }
 
 }
