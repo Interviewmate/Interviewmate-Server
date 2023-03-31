17966e2e0600a435950f229fd839d6e43cc871da
parkrootseok <parkrootseok@gmail.com>
2023-04-01 01:37:25 +0900

    [Feature] #24 - 인증 번호 전송 및 검증 기능
    - 이메일 인증 번호를 전송하는 기능 구현
    - 받은 인증 번호를 검증하는 기능 구현

--- src/main/java/org/interviewmate/domain/auth/api/AuthApiController.java
+++ src/main/java/org/interviewmate/domain/auth/api/AuthApiController.java
@@ -1,17 +1,23 @@
 package org.interviewmate.domain.auth.api;
 
+import static org.interviewmate.global.util.response.ResponseCode.SUCCESS;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.interviewmate.domain.auth.dto.request.LoginReq;
 import org.interviewmate.domain.auth.dto.reponse.LoginRes;
+import org.interviewmate.global.util.response.ResponseUtil;
+import org.interviewmate.global.util.response.dto.ResponseDto;
 import org.interviewmate.infra.email.EmailService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "인증 관련 API")
@@ -28,11 +34,30 @@ public class AuthApiController {
         return null;
     }
 
-    @Operation(summary = "이메일 인증 API", description = "회원 가입 시 이메일 인증 진행")
+    @Operation(summary = "메일 인증 코드 발송 API", description = "회원 가입 시 이메일 인증 코드 발송")
+    @Parameter(name = "toEmail", description = "이메일", example = "parkrootseok@gmail.com")
     @GetMapping("/email")
-    public String AuthenticateEmail(@Parameter(description = "이메일", example = "parkrootseok@gmail.com") @Email String email) {
+    public ResponseDto<String> sendAuthenticationCode(@RequestParam @Email String toEmail) {
 
-       return emailService.authenticateEmail(email);
+        emailService.sendEmail(toEmail);
+        return ResponseUtil.SUCCESS(SUCCESS, "이메일을 확인하세요.");
 
     }
+
+    @Operation(summary = "메일 인증 코드 검증 API", description = "발송한 인증 코드 검증")
+    @Parameters({
+            @Parameter(name = "email", description = "이메일", example = "parkrootseok@gmail.com"),
+            @Parameter(name = "code", description = "인증 코드", example = "53085")
+    })
+    @GetMapping("/authCode")
+    private ResponseDto<String> verifyAuthenticationCode(@RequestParam @Email String email, @RequestParam String code) {
+
+        if(emailService.verifyEmailCode(email, code)) {
+            return ResponseUtil.SUCCESS(SUCCESS, "인증 완료.");
+        }
+
+        return ResponseUtil.SUCCESS(SUCCESS, "인증 실패.");
+
+    }
+
 }
--- src/main/java/org/interviewmate/infra/email/EmailService.java
+++ src/main/java/org/interviewmate/infra/email/EmailService.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.interviewmate.infra.redis.RedisService;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -15,30 +16,53 @@ public class EmailService {
     private final String EMAIL_TITLE = "[InterviewMate] 인증 번호 입니다.";
     private final String EMAIL_CONTENT = "인증 번호 : ";
 
-    private final JavaMailSender javaMailSender;
+    private final JavaMailSender mailSender;
+    private final RedisService redisService;
 
-    public String authenticateEmail(String email) {
+    public String sendEmail(String toEmail) {
 
-        Random random = new Random();
-        String authenticationKey = String.valueOf(random.nextInt(88888) + 11111);
+       if (!redisService.existData(toEmail)) {
+           redisService.deleteData(toEmail);
+       }
 
-        sendAuthenticationEmail(email, authenticationKey);
-        return authenticationKey;
+       String code = createAuthCode();
+
+       try {
+           MimeMessage message = createEmailForm(toEmail, code);
+           mailSender.send(message);
+       } catch (MessagingException e) {
+           e.printStackTrace();
+       }
+
+       return "이메일을 확인하세요.";
+
     }
 
-    private void sendAuthenticationEmail(String email, String authenticationKey) {
+    private String createAuthCode() {
+        Random random = new Random();
+        return String.valueOf(random.nextInt(88888) + 11111);
+    }
 
-        try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            helper.setTo(email);
-            helper.setSubject(EMAIL_TITLE);
-            helper.setText(EMAIL_CONTENT + authenticationKey, true);
-            javaMailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            e.printStackTrace();
+    private MimeMessage createEmailForm(String toEmail, String authCode) throws MessagingException{
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+        helper.setTo(toEmail);
+        helper.setSubject(EMAIL_TITLE);
+        helper.setText(EMAIL_CONTENT + authCode, true);
+
+        redisService.setDataExpire(toEmail, authCode, 60 * 5L);
+
+        return message;
+
+    }
+
+    public Boolean verifyEmailCode(String email, String code) {
+        String findCode = redisService.getData(email);
+        if (findCode == null) {
+            return false;
         }
-
+        return findCode.equals(code);
     }
 
 }
