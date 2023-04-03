9aded067c193744c899e32e1c10137e81aaa81aa
parkrootseok <parkrootseok@gmail.com>
2023-04-03 18:07:41 +0900

    [Add] #14 - 중복 닉네임 검증 로직

--- src/main/java/org/interviewmate/global/error/ErrorCode.java
+++ src/main/java/org/interviewmate/global/error/ErrorCode.java
@@ -18,7 +18,8 @@ public enum ErrorCode {
 
     NOT_FOUND_DATA(HttpStatus.NOT_FOUND, "해당하는 데이터를 찾을 수 없습니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다." ),
-    EMPTY_KEYWORD(HttpStatus.BAD_REQUEST, "키워드를 입력해주세요.");
+    EMPTY_KEYWORD(HttpStatus.BAD_REQUEST, "키워드를 입력해주세요."),
+    EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다.");
 
     private final HttpStatus code;
     private final String message;
