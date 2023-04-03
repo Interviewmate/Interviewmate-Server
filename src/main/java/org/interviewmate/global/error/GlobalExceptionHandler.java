d3b170f971c9652c1d27f488cafe84ea400f2f61
LimGayoung <ygy920@gmail.com>
2023-04-04 00:43:37 +0900

    [Add]#22 - exception handler 추가
    MissingServletRequestParameterException, HttpMessageNotReadableException

--- src/main/java/org/interviewmate/global/error/GlobalExceptionHandler.java
+++ src/main/java/org/interviewmate/global/error/GlobalExceptionHandler.java
@@ -6,9 +6,11 @@ import org.interviewmate.global.error.exception.CustomException;
 import org.interviewmate.global.util.response.ResponseUtil;
 import org.interviewmate.global.util.response.dto.ResponseDto;
 import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -52,4 +54,16 @@ public class GlobalExceptionHandler {
     protected ResponseDto<ErrorResponseDto> handleConversionFailedException(ConversionFailedException e) {
         return ResponseUtil.ERROR(ErrorCode.BAD_REQUEST, ErrorResponseDto.of(e.getMessage()));
     }
+
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    protected ResponseDto<ErrorResponseDto> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        return ResponseUtil.ERROR(ErrorCode.BAD_REQUEST, ErrorResponseDto.of(e.getMessage()));
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    protected ResponseDto<ErrorResponseDto> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseUtil.ERROR(ErrorCode.BAD_REQUEST, ErrorResponseDto.of(e.getMessage()));
+    }
+
+
 }
