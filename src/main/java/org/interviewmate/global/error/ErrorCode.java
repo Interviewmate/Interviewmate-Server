8eb3487017fea5ed5cb60ab31e370fcd8f205039
LimGayoung <ygy920@gmail.com>
2023-04-04 09:44:49 +0900

    [Fix]#22 - 주석 추가
    common, interview, user error 주석으로 구분

--- src/main/java/org/interviewmate/global/error/ErrorCode.java
+++ src/main/java/org/interviewmate/global/error/ErrorCode.java
@@ -16,11 +16,19 @@ public enum ErrorCode {
      *  404 : 존재하지 않는 정보에 대한 요청.
      */
 
+    //common error
     NOT_FOUND_DATA(HttpStatus.NOT_FOUND, "해당하는 데이터를 찾을 수 없습니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다." ),
+
+    //user error
     EMPTY_KEYWORD(HttpStatus.BAD_REQUEST, "키워드를 입력해주세요."),
-    INTERVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 인터뷰가 없습니다"),
-    EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다.");
+    EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다."),
+
+    //interview error
+    INTERVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 인터뷰가 없습니다");
+
+
+
 
     private final HttpStatus code;
     private final String message;
