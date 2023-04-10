5fa551fd27d9205308a9fbc2ca5d86ef091d6e0a
parkrootseok <parkrootseok@gmail.com>
2023-04-11 00:14:28 +0900

    [Add] #31 - jwt관련 OpenApi 설정

--- src/main/java/org/interviewmate/global/config/openApi/OpenApiConfig.java
+++ src/main/java/org/interviewmate/global/config/openApi/OpenApiConfig.java
@@ -1,15 +1,19 @@
 package org.interviewmate.global.config.openApi;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.Arrays;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class        OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
@@ -22,7 +26,18 @@ public class OpenApiConfig {
         localServer.setDescription("local");
         localServer.setUrl("http://localhost:8082");
 
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Authorization", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .info(getInfo())
                 .servers(Arrays.asList(devServer, localServer));
 
@@ -32,6 +47,5 @@ public class OpenApiConfig {
                 .title("InterviewMate REST API")
                 .description("InterviewMate API DOCS");
     }
-
 }
 
