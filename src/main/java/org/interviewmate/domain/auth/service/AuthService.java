35dbb00815983cf68b825920cf8cf4574375635d
parkrootseok <parkrootseok@gmail.com>
2023-04-12 15:47:02 +0900

    [Add] #33 - login시 refresh token 발급

--- src/main/java/org/interviewmate/domain/auth/service/AuthService.java
+++ src/main/java/org/interviewmate/domain/auth/service/AuthService.java
@@ -1,8 +1,10 @@
 package org.interviewmate.domain.auth.service;
 
+import static org.interviewmate.global.error.ErrorCode.FAIL_TO_LOGIN;
 import static org.interviewmate.global.error.ErrorCode.NOT_EXIST_USER;
 import static org.interviewmate.global.error.ErrorCode.WRONG_PASSWORD;
 import static org.interviewmate.global.util.encrypt.Secret.PASSWORD_KEY;
+import static org.interviewmate.global.util.encrypt.jwt.model.TokenType.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,7 @@ import org.interviewmate.domain.auth.model.dto.request.LoginReq;
 import org.interviewmate.domain.user.exception.UserException;
 import org.interviewmate.domain.user.model.User;
 import org.interviewmate.domain.user.repository.UserRepository;
+import org.interviewmate.global.util.encrypt.jwt.model.TokenType;
 import org.interviewmate.global.util.encrypt.password.AES128;
 import org.interviewmate.global.util.encrypt.jwt.service.JwtService;
 import org.springframework.stereotype.Service;
@@ -29,18 +32,24 @@ public class AuthService {
 
         // 이메일로 User 찾기
         User user = userRepository.findByEmail(loginReq.getEmail())
-                .orElseThrow(() -> new UserException(NOT_EXIST_USER));
+                .orElseThrow(() -> new UserException(FAIL_TO_LOGIN));
 
         // 찾은 유저에서 비밀번호 추출
         String password = new AES128(PASSWORD_KEY).decrypt(user.getPassword());
 
         // 비밀번호 일치 확인
         if (!password.equals(loginReq.getPassword())) {
-            throw new UserException(WRONG_PASSWORD);
+            throw new UserException(FAIL_TO_LOGIN);
         }
 
         // 토큰 발급
-        String accessToken = jwtService.createAccessToken(user.getEmail(), user.getRoles());
+        String accessToken = jwtService.createToken(user.getEmail(), user.getRoles(), ACCESS_TOKEN);
+
+        if (user.getRefreshToken() == null) {
+            String refreshToken = jwtService.createToken(user.getEmail(), user.getRoles(), REFRESH_TOKEN);
+            user.setRefreshToken(refreshToken);
+            userRepository.save(user);
+        }
 
         return LoginRes.of(user, accessToken);
 
