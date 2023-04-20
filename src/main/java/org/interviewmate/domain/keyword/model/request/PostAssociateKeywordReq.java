ac9b2a3f9dcae9c6f7c76335e3dfbc7941ff2729
parkrootseok <parkrootseok@gmail.com>
2023-04-20 11:32:27 +0900

    [Chore] #14 - 키워드 예시 변경
    - 대문자로 통일하여 사용

--- src/main/java/org/interviewmate/domain/keyword/model/request/PostAssociateKeywordReq.java
+++ src/main/java/org/interviewmate/domain/keyword/model/request/PostAssociateKeywordReq.java
@@ -12,7 +12,7 @@ public class PostAssociateKeywordReq {
     @Schema(description = "유저 식별자")
     private Long userId;
 
-    @Schema(description = "키워드", example = "[\"Spring\", \"JPA\", \"Java\"]")
+    @Schema(description = "키워드", example = "[\"SPRING\", \"JPA\", \"JAVA\"]")
     private List<@NotBlank(message = "기술 스택, 언어를 선택하세요.") String> keywords;
 
 }
