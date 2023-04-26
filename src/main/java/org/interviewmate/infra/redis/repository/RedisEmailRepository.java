585669adb36c3fb7cb8ff43be6846fd424e5d25d
parkrootseok <parkrootseok@gmail.com>
2023-04-26 17:31:54 +0900

    [Add] #1 - Redis Configuration

---
+++ src/main/java/org/interviewmate/infra/redis/repository/RedisEmailRepository.java
@@ -0,0 +1,49 @@
+package org.interviewmate.infra.redis.repository;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisEmailRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 데이터 생성 (유효 시간 설정 포함)
+     */
+    public void saveWithExpirationDate(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    /**
+     * 키 값으로 데이터 가져오기
+     */
+    public String findByKey(String key) {
+
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+
+    }
+
+    /**
+     * 키 값으로 데이터 유무 확인
+     */
+    public boolean isExist(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+
+    /**
+     * 데이터 삭제
+     */
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+}
