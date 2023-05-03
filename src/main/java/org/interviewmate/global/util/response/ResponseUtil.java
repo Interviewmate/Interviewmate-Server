b840ad9e00293c1192f409cab320d1a8c93a4cbe
parkrootseok <parkrootseok@gmail.com>
2023-05-03 15:51:09 +0900

    [Refactor] #46 - Response 수정
    - 빌더 패턴 적용

--- src/main/java/org/interviewmate/global/util/response/ResponseUtil.java
+++ src/main/java/org/interviewmate/global/util/response/ResponseUtil.java
@@ -1,14 +1,30 @@
 package org.interviewmate.global.util.response;
 
+import static org.interviewmate.global.util.response.ResponseStatus.FAILURE;
+import static org.interviewmate.global.util.response.ResponseStatus.SUCCESS;
+
 import org.interviewmate.global.util.response.dto.ResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseUtil {
-    public static <T>ResponseEntity<ResponseDto<T>> SUCCESS(ResponseCode code, T data) {
-        return new ResponseEntity(new ResponseDto(ResponseStatus.SUCCESS, code.getMessage(), data), code.getCode());
+    public static <T> ResponseEntity<ResponseDto<T>> SUCCESS(ResponseCode code, T data) {
+        return new ResponseEntity(
+                ResponseDto.builder()
+                        .status(SUCCESS)
+                        .message(code.getMessage())
+                        .result(data)
+                        .build()
+                , code.getCode());
     }
 
-    public static <T>ResponseEntity<ResponseDto<T>> FAILURE(ResponseCode code, T data) {
-        return new ResponseEntity(new ResponseDto(ResponseStatus.FAILURE, code.getMessage(), data), code.getCode());
+    public static <T> ResponseEntity<ResponseDto<T>> FAILURE(ResponseCode code, T data) {
+        return new ResponseEntity(
+                ResponseDto.builder()
+                        .status(FAILURE)
+                        .message(code.getMessage())
+                        .result(data)
+                        .build()
+                , code.getCode());
     }
+
 }
