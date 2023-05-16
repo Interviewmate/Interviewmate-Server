90b7dd0584656922a77c048f0a244addb1269f2a
LimGayoung <ygy920@gmail.com>
2023-05-17 01:15:14 +0900

    [Fix]#47 - @Value 재사용

--- src/main/java/org/interviewmate/domain/portfolio/service/PortfolioService.java
+++ src/main/java/org/interviewmate/domain/portfolio/service/PortfolioService.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
     private final UserRepository userRepository;
-//    @Value("${ai.portfolio.keyword}") //todo : 배포 오류로 인해 주석처리함
+    @Value("${ai.portfolio.keyword}")
     private String keywordUri = "localhost:5000/keyword";
 
     // todo: createPortfolio는 debug 위한 메서드. s3 service에서 구현되면 삭제해야 함
