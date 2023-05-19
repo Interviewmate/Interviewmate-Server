6b4159fa5cb8ad8900d9fc145d9d868f3a2df83d
parkrootseok <parkrootseok@gmail.com>
2023-05-19 15:00:55 +0900

    [Chore] #49 - Swagger 설정 추가

--- src/main/java/org/interviewmate/domain/analysis/api/AnalysisController.java
+++ src/main/java/org/interviewmate/domain/analysis/api/AnalysisController.java
@@ -2,6 +2,10 @@ package org.interviewmate.domain.analysis.api;
 
 import static org.interviewmate.global.util.response.ResponseCode.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.interviewmate.domain.analysis.service.BehaviorAnalysisService;
 import org.interviewmate.domain.analysis.service.GazeAnalysisService;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Tag(name = "분석 관련 API")
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/analyses")
@@ -22,6 +28,11 @@ public class AnalysisController {
     private final GazeAnalysisService gazeAnalysisService;
     private final BehaviorAnalysisService behaviorAnalysisService;
 
+    @Operation(summary = "분석 요청 API", description = "1개 면접 질문 영상에 대한 시선, 자세, 답변 분석 요청")
+    @Parameters({
+            @Parameter(name = "interviewId", description = "인터뷰 식별자"),
+            @Parameter(name = "objectKey", description = "객체 키")
+    })
     @GetMapping("/{interviewId}")
     public ResponseEntity<ResponseDto<String>> createAnalysis(@PathVariable Long interviewId, @RequestParam String objectKey) {
 
