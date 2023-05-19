60da051674af8cc6b32ecc4b5bf2b47c25f2420b
LimGayoung <ygy920@gmail.com>
2023-05-19 17:14:15 +0900

    [Fix]#22 - swagger 수정, 로직 수정
    UserService > UserRepository

--- src/main/java/org/interviewmate/domain/interview/model/dto/request/InterviewFindDailyRequestDto.java
+++ src/main/java/org/interviewmate/domain/interview/model/dto/request/InterviewFindDailyRequestDto.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class InterviewFindDailyRequestDto {
-    @Schema(description = "사용자 id")
+    @Schema(description = "사용자 id", example = "1")
     @NotNull
     private Long userId;
 
