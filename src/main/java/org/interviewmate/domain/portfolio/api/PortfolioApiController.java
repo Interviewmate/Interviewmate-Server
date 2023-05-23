82f6452af9905156c46a0cdfe26f7ee050c8bfd6
LimGayoung <ygy920@gmail.com>
2023-05-23 09:36:04 +0900

    [Chore]#69 - 스웨거 추가

--- src/main/java/org/interviewmate/domain/portfolio/api/PortfolioApiController.java
+++ src/main/java/org/interviewmate/domain/portfolio/api/PortfolioApiController.java
@@ -1,6 +1,8 @@
 package org.interviewmate.domain.portfolio.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,9 @@ public class PortfolioApiController {
     @Operation(summary = "포트폴리오 키워드 추출 요청", description = "pathVariable (Long userId) 을 이용해 포트폴리오에서 키워드를 추출합니다.", responses = {
             @ApiResponse(responseCode = "200", description = "포트폴리오 키워드 추출 성공")
     })
+    @Parameters({
+            @Parameter(name = "userId", description = "user의 id", example = "1"),
+    })
     @GetMapping("/keyword/{userId}")
     public ResponseEntity<ResponseDto<String>> getPortfolioKeyword(@PathVariable("userId") Long userId) {
         portfolioService.getKeyword(new PortfolioGetKeywordRequestDto(userId));
@@ -33,6 +38,9 @@ public class PortfolioApiController {
 
     @Operation(summary = "포트폴리오 유무 확인 요청", description = "pathVariable (Long userId) 을 이용해 해당 유저에게 포트폴리오가 있는지 확인합니다.", responses = {
             @ApiResponse(responseCode = "200", description = "포트폴리오 유무 확인 성공")
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "user의 id", example = "1"),
     })
     @GetMapping("/{userId}")
     public ResponseEntity<ResponseDto<PortfolioCheckExisitResponseDto>> checkPortfolioIsExist(@PathVariable("userId") Long userId) {
